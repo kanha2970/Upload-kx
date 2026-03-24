6dac8e2ab9250fbb9c96d956771ee595b39203bf
creator154 <cteator898@gmail.com>
2026-03-24 17:07:00 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -29,8 +29,8 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 # Initialize the bot
 bot = Client(
     "bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
+    api_id=34943077,
+    api_hash="11aeec678349456f1d190f02975ed89f",
     bot_token=BOT_TOKEN
 )
 
